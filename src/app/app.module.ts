6b6710dbe1c0c0f752e515dc4a7ec11b508c515c
essaghir.e <essaghir.e@gmail.com>
2020-05-16 18:50:55 -0400

    recipes ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes/recipes-list//recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
     ShoppingListComponent,
     HeaderComponent,
     RecipesListComponent,
-    RecipesDetailComponent
+    RecipesDetailComponent,
+    RecipesItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../model/recipe';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test', 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/17/16/asset/buzzfeed-prod-web-02/sub-buzz-11594-1545083892-7.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*'),
+    new Recipe('A test recipe', 'test', 'https://www.foxandbriar.com/wp-content/uploads/2019/04/Creamy-Shrimp-with-basil-and-roasted-Red-peppers-3-of-7.jpg'),
+    new Recipe('A test recipe', 'test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gimmesomeoven.com%2Fwp-content%2Fuploads%2F2018%2F12%2FSalsa-Verde-Steak-Potato-Soup-Recipe-2.jpg&f=1&nofb=1'),
+    new Recipe('A test recipe', 'test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.simplyrecipes.com%2Fwp-content%2Fuploads%2F2012%2F05%2Fsmoked-salmon-hash-vertical-a-1600.jpg&f=1&nofb=1')
+  ];
 
   constructor() { }
 
