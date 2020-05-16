1f7ec6ca40204d580f8f57a258bd28b07aa11952
essaghir.e <essaghir.e@gmail.com>
2020-05-16 00:29:15 -0400

    new cmpnts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipesListComponent,
+    RecipesDetailComponent
   ],
   imports: [
     BrowserModule
